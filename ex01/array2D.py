5b4f6e4882eb5e37ab9154547c75b5cf4c9f8e09
João <jocolognesi@gmail.com>
2026-09-10 16:57:19 -0300

    default commit message

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -5,15 +5,19 @@ def slice_me(family: list, start: int, end: int) -> list:
     """Slices a 2D list"""
     try:
         if not isinstance(start, int) or not isinstance(end, int):
-            raise TypeError("Star and end must be int")
-        if not isinstance(family, list):
+            raise TypeError("star and end must be int")
+        if not isinstance(family, list) or len(family) == 0:
             raise TypeError("Input must be a list")
+        if not all(isinstance(line, list) for line in family):
+            raise TypeError("Input must be a list of lists")
         for line in family:
             if len(line) != len(family[0]):
                 raise AssertionError("Input list with different sizes")
-        print(f"My shape is : {np.array(family).shape}")
+        arr = np.array(family)
+        print(f"My shape is : {arr.shape}")
+        sliced = arr[start:end]
         print(f"My new shape is : {np.array(family)[start:end].shape}")
-        return np.array(family)[start:end].tolist()
-    except AssertionError as error:
+        return sliced.tolist()
+    except (AssertionError, TypeError, IndexError) as error:
         print("Error:", error)
-        return ""
+        return[]
