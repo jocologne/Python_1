9244e191248e814802055fbabe390c92e85aa107
João <jocolognesi@gmail.com>
2026-09-10 18:48:13 -0300

    default commit message

--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,9 +1,22 @@
 from load_image import ft_load
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
     """Display info and zoom in image"""
-    print(ft_load("animal.jpeg"))
+    try:
+        img = ft_load("animal.jpeg")
+        print(img)
+        h = img.shape[0]
+        w = img.shape[1]
+        zoomed = img[h // 4: h // 4 + 400, w // 4: w // 4 + 400]
+        print(f"New shape after slicing: {zoomed.shape}")
+        plt.imshow(zoomed.squeeze())
+        plt.savefig("zoomed.png")
+        print(np.array(zoomed))
+    except Exception as error:
+        print(f"Error: {error}")
 
 
 if __name__ == "__main__":
